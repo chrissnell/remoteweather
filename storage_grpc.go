1a196af6880727825a0993d3a05c62ebe2159772
Chris Snell <chris.snell@gmail.com>
2021-12-19 22:53:01 -0500

    Add support for querying DB

--- storage_grpc.go
+++ storage_grpc.go
@@ -5,12 +5,18 @@ import (
 	"fmt"
 	"net"
 	"sync"
+	"time"
 
 	weather "github.com/chrissnell/gopherwx/protobuf"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 // GRPCConfig describes the YAML-provided configuration for a gRPC
@@ -23,12 +29,11 @@ type GRPCConfig struct {
 
 // GRPCStorage implements a gRPC storage backend
 type GRPCStorage struct {
-	GRPCServer      *grpc.Server
-	Listener        net.Listener
-	RPCReadingChan  chan Reading
-	Ctx             context.Context
 	ClientChans     []chan Reading
 	ClientChanMutex sync.RWMutex
+	DB              *gorm.DB
+	DBEnabled       bool
+
 	weather.UnimplementedWeatherServer
 }
 
@@ -36,7 +41,6 @@ type GRPCStorage struct {
 // them off to our gRPC clients
 func (g *GRPCStorage) StartStorageEngine(ctx context.Context, wg *sync.WaitGroup) chan<- Reading {
 	log.Info("starting gRPC storage engine...")
-	g.Ctx = ctx
 	readingChan := make(chan Reading)
 	go g.processMetrics(ctx, wg, readingChan)
 	return readingChan
@@ -50,8 +54,9 @@ func (g *GRPCStorage) processMetrics(ctx context.Context, wg *sync.WaitGroup, rc
 		select {
 		case r := <-rchan:
 			g.ClientChanMutex.RLock()
+			// Send the Reading we just received to all client channels.
+			// If there are no clients connected, it gets discarded.
 			for _, v := range g.ClientChans {
-				// log.Infof("Sending reading to client #%v", i)
 				v <- r
 			}
 			g.ClientChanMutex.RUnlock()
@@ -65,9 +70,8 @@ func (g *GRPCStorage) processMetrics(ctx context.Context, wg *sync.WaitGroup, rc
 // NewGRPCStorage sets up a new gRPC storage backend
 func NewGRPCStorage(c *Config) (*GRPCStorage, error) {
 	var err error
-	g := GRPCStorage{}
-
-	g.RPCReadingChan = make(chan Reading, 10)
+	var s *grpc.Server
+	var g GRPCStorage
 
 	if c.Storage.GRPC.Cert != "" && c.Storage.GRPC.Key != "" {
 		// Create the TLS credentials
@@ -75,23 +79,60 @@ func NewGRPCStorage(c *Config) (*GRPCStorage, error) {
 		if err != nil {
 			return &GRPCStorage{}, fmt.Errorf("could not create TLS server from keypair: %v", err)
 		}
-		g.GRPCServer = grpc.NewServer(grpc.Creds(creds))
+		s = grpc.NewServer(grpc.Creds(creds))
 	} else {
-		g.GRPCServer = grpc.NewServer()
+		s = grpc.NewServer()
 	}
+
+	// Optionally, add gRPC reflection to our servers so that clients can self-discover
+	// our methods.
+	reflection.Register(s)
 
 	listenAddr := fmt.Sprintf(":%v", c.Storage.GRPC.Port)
 
-	g.Listener, err = net.Listen("tcp", listenAddr)
+	l, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		return &GRPCStorage{}, fmt.Errorf("could not create gRPC listener: %v", err)
 	}
 
-	weather.RegisterWeatherServer(g.GRPCServer, &g)
+	// If a TimescaleDB database was configured, set up a GORM DB handle so that the
+	// gRPC handlers can retrieve data
+	if c.Storage.TimescaleDB.ConnectionString != "" {
+		err = g.connectToDatabase(c.Storage.TimescaleDB.ConnectionString)
+		if err != nil {
+			return &GRPCStorage{}, fmt.Errorf("gRPC storage could not connect to database: %v", err)
+		}
+		g.DBEnabled = true
+	}
 
-	go g.GRPCServer.Serve(g.Listener)
+	weather.RegisterWeatherServer(s, &g)
+
+	go s.Serve(l)
 
 	return &g, nil
+}
+
+func (g *GRPCStorage) connectToDatabase(dbURI string) error {
+	var err error
+	// Create a logger for gorm
+	dbLogger := logger.New(
+		zap.NewStdLog(zapLogger),
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Warn, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+
+	log.Info("connecting to TimescaleDB for gRPC data backend...")
+	g.DB, err = gorm.Open(postgres.Open(dbURI), &gorm.Config{Logger: dbLogger})
+	if err != nil {
+		log.Warn("warning: unable to create a TimescaleDB connection:", err)
+		return err
+	}
+
+	return nil
 }
 
 // registerClient creates a channel for sending readings to a client and adds it
@@ -112,7 +153,50 @@ func (g *GRPCStorage) deregisterClient(i int) {
 	g.ClientChans = g.ClientChans[:len(g.ClientChans)-1]
 }
 
-// GetLiveWeather satisfies the implementation of weather.WeatherServer
+func (g *GRPCStorage) GetWeatherSpan(ctx context.Context, request *weather.WeatherSpanRequest) (*weather.WeatherSpan, error) {
+	var dbFetchedReadings []Reading
+
+	spanStart := time.Now().Add(-request.SpanDuration.AsDuration())
+
+	if g.DBEnabled {
+		g.DB.Table("weather").Where("time > ?", spanStart).Find(&dbFetchedReadings)
+		log.Infof("returned rows: %v", len(dbFetchedReadings))
+
+		span := &weather.WeatherSpan{
+			SpanStart: (*timestamppb.Timestamp)(timestamppb.New(spanStart)),
+			Reading:   g.transformReadings(&dbFetchedReadings)}
+
+		log.Infof("getweatherspan -> spanDuration: %v", request.SpanDuration.AsDuration())
+
+		return span, nil
+	}
+
+	return &weather.WeatherSpan{}, fmt.Errorf("ignoring GetWeatherSpan request: database not configured")
+}
+
+func (g *GRPCStorage) transformReadings(dbReadings *[]Reading) []*weather.WeatherReading {
+	grpcReadings := make([]*weather.WeatherReading, 0)
+
+	for _, r := range *dbReadings {
+		grpcReadings = append(grpcReadings, &weather.WeatherReading{
+			ReadingTimestamp:   (*timestamppb.Timestamp)(timestamppb.New(r.Timestamp)),
+			OutsideTemperature: r.OutTemp,
+			OutsideHumidity:    int32(r.OutHumidity),
+			Barometer:          r.Barometer,
+			WindSpeed:          int32(r.WindSpeed),
+			WindDirection:      int32(r.WindDir),
+			RainfallDay:        r.DayRain,
+			WindChill:          r.Windchill,
+			HeatIndex:          r.HeatIndex,
+			InsideTemperature:  r.InTemp,
+			InsideHumidity:     int32(r.InHumidity),
+		})
+	}
+
+	return grpcReadings
+}
+
+// GetLiveWeather implements the live weather feed for WeatherServer
 func (g *GRPCStorage) GetLiveWeather(e *weather.Empty, stream weather.Weather_GetLiveWeatherServer) error {
 	ctx := stream.Context()
 	p, _ := peer.FromContext(ctx)
@@ -135,13 +219,15 @@ func (g *GRPCStorage) GetLiveWeather(e *weather.Empty, stream weather.Weather_Ge
 			rts := timestamppb.New(r.Timestamp)
 
 			stream.Send(&weather.WeatherReading{
-				LastReading:     rts,
-				OutsideTemp:     r.OutTemp,
-				OutsideHumidity: int32(r.OutHumidity),
-				Barometer:       r.Barometer,
-				WindSpeed:       int32(r.WindSpeed),
-				WindDir:         int32(r.WindDir),
-				RainfallDay:     r.DayRain,
+				ReadingTimestamp:   rts,
+				OutsideTemperature: r.OutTemp,
+				InsideTemperature:  r.InTemp,
+				OutsideHumidity:    int32(r.OutHumidity),
+				InsideHumidity:     int32(r.InHumidity),
+				Barometer:          r.Barometer,
+				WindSpeed:          int32(r.WindSpeed),
+				WindDirection:      int32(r.WindDir),
+				RainfallDay:        r.DayRain,
 			})
 
 		}
