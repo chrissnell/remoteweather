d39dec4a5cd7aba1e3105e1b7830c093b8db9c38
Chris Snell <1072626+chrissnell@users.noreply.github.com>
2025-07-02 11:16:47 -0600

    feat: optionally serve assets from local fs while developing

--- internal/controllers/management/assets.go
+++ internal/controllers/management/assets.go
@@ -3,6 +3,7 @@ package management
 import (
 	"embed"
 	"io/fs"
+	"os"
 )
 
 // Embed the management interface assets
@@ -12,6 +13,16 @@ var assetsFS embed.FS
 
 // GetAssets returns the embedded assets filesystem
 func GetAssets() fs.FS {
+	// If the REMOTEWEATHER_ASSETS_DIR environment variable is set and points to a
+	// valid directory, serve assets directly from the file-system.  This is
+	// useful during development because it removes the need to recompile the
+	// binary every time a CSS/JS/HTML file is tweaked.
+	if dir := os.Getenv("REMOTEWEATHER_ASSETS_DIR"); dir != "" {
+		if info, err := os.Stat(dir); err == nil && info.IsDir() {
+			return os.DirFS(dir)
+		}
+	}
+
 	// Return a sub-filesystem starting from the "assets" directory
 	assets, err := fs.Sub(assetsFS, "assets")
 	if err != nil {
