bd9797ab7dd387f5483546d92a98f52604fccf37
Chris Snell <1072626+chrissnell@users.noreply.github.com>
2025-07-05 15:37:20 -0600

    Improve coordinate handling and fix weather station save modal issues

--- internal/controllers/management/assets.go
+++ internal/controllers/management/assets.go
@@ -13,11 +13,11 @@ var assetsFS embed.FS
 
 // GetAssets returns the embedded assets filesystem
 func GetAssets() fs.FS {
-	// If the REMOTEWEATHER_ASSETS_DIR environment variable is set and points to a
+	// If the REMOTEWEATHER_MANAGEMENT_ASSETS_DIR environment variable is set and points to a
 	// valid directory, serve assets directly from the file-system.  This is
 	// useful during development because it removes the need to recompile the
 	// binary every time a CSS/JS/HTML file is tweaked.
-	if dir := os.Getenv("REMOTEWEATHER_ASSETS_DIR"); dir != "" {
+	if dir := os.Getenv("REMOTEWEATHER_MANAGEMENT_ASSETS_DIR"); dir != "" {
 		if info, err := os.Stat(dir); err == nil && info.IsDir() {
 			return os.DirFS(dir)
 		}
